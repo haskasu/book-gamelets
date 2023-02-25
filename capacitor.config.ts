75c77a04bae626f22173df3fa7feefc00ee51f0e
haskasu <haskasu@gmail.com>
2023-02-25 13:32:16 -0500

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gamelets.app',
+  appName: 'Gamelets',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
