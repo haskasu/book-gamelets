50e58ddc90494cc68fbec246c156391e4c2beaf9
haskasu <haskasu@gmail.com>
2024-01-20 07:08:08 -0500

    fix Slongshot does not init releaseStart on mouseEndDrag

--- src/castle-falls/Slingshot.ts
+++ src/castle-falls/Slingshot.ts
@@ -36,6 +36,8 @@ export class Slingshot {
             // 改變石頭的類別
             const rock = this.shootData.rock;
             rock.collisionFilter.category = 0b01;
+            // 記錄發射石頭的起始位置
+            this.shootData.releaseStart = Vector.clone(rock.position);
             // 播放發射音效
             playSound(shootSnd);
             // 等待石頭飛得夠遠
